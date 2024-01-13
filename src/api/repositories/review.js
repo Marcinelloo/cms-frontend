b6859780593047807afe0b65494d5da3f5e47ea1
Michał Januszewski <michaljn2@gmail.com>
2024-01-13 19:51:20 +0100

    Improve reviews

--- src/api/repositories/review.js
+++ src/api/repositories/review.js
@@ -3,8 +3,24 @@ import store from "../store";
 const queryString = require("query-string");
 const GROUP_DEFAULT_URL = "reviews?populate=user";
 
-export const findAllReviews = async () => {
-  return await API.get(`${GROUP_DEFAULT_URL}`);
+export const findAllReviews = async (pageNumber) => {
+  const user = store.getUser();
+  if (user) {
+    return await API.get(
+      `${GROUP_DEFAULT_URL}&filters[user][id][$ne]=${user.id}&sort=Title&pagination[page]=${pageNumber}&pagination[pageSize]=1`
+    );
+  } else {
+    return await API.get(
+      `${GROUP_DEFAULT_URL}&sort=Title&pagination[page]=${pageNumber}&pagination[pageSize]=1`
+    );
+  }
+};
+
+export const findUserReview = async () => {
+  const user = store.getUser();
+  return await API.get(
+    `${GROUP_DEFAULT_URL}&filters[user][id][$eq]=${user.id}`
+  );
 };
 
 export const createReview = async (values) => {
