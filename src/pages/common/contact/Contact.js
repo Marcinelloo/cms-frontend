e7a0b38f74a66ae74c5f92bdb457b13c344c2c6c
marcinekk <marcinekk@MBP-marcinekk.ht.home>
2024-01-15 20:51:39 +0100

    up

--- src/pages/common/contact/Contact.js
+++ src/pages/common/contact/Contact.js
@@ -5,6 +5,7 @@ const PageContainer = styled.div`
   max-width: 800px;
   margin: 0 auto;
   padding: 20px;
+  height: 100%;
 `;
 
 const Section = styled.section`
@@ -23,25 +24,45 @@ const Contact = () => {
   return (
     <PageContainer>
       <Section>
-        <Heading>Contact Us</Heading>
+        <Heading>Kontakt</Heading>
         <Paragraph>
-          Feel free to reach out to us if you have any questions or inquiries.
+          Witaj w naszej wypożyczalni samochodów, gdzie podróżowanie staje się
+          przyjemnością! Jesteśmy gotowi odpowiedzieć na Twoje pytania i
+          zapewnić niezapomniane doświadczenie podróżowania.
         </Paragraph>
       </Section>
       <Section>
-        <Heading>Contact Information</Heading>
+        <Heading>Dlaczego My?</Heading>
+        <Paragraph>
+          Nasza wypożyczalnia oferuje nie tylko samochody, ale także unikalne
+          doświadczenie podróży. Oferujemy szeroki wybór luksusowych pojazdów,
+          które pozwolą Ci podróżować z klasą. Nasza flota jest zawsze utrzymana
+          w najwyższym standardzie, aby zapewnić bezpieczeństwo i komfort.
+        </Paragraph>
+      </Section>
+      <Section>
+        <Heading>Dane Kontaktowe</Heading>
         <Paragraph>
           Email: info@cardealership.com
           <br />
-          Phone: (123) 456-7890
+          Telefon: (123) 456-7890
         </Paragraph>
       </Section>
       <Section>
-        <Heading>Visit Us</Heading>
+        <Heading>Odwiedź Nas</Heading>
         <Paragraph>
-          123 Main Street
-          <br />
-          Cityville, State 12345
+          Znajdziesz nas pod adresem: 123 Ulica Główna, Miastowo, Województwo
+          12345. Nie wahaj się nas odwiedzić i dowiedzieć się więcej o naszych
+          usługach.
+        </Paragraph>
+      </Section>
+      <Section>
+        <Heading>Dodatkowe Informacje</Heading>
+        <Paragraph>
+          Chcemy, aby Twoje podróże były nie tylko wygodne, ale także pełne
+          przygód. Sprawdź naszą stronę internetową, aby dowiedzieć się o
+          najnowszych promocjach, trasach podróży i unikalnych ofertach dla
+          naszych klientów.
         </Paragraph>
       </Section>
     </PageContainer>
