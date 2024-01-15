39a959b67056516e541e4844bee77c746908bb88
Jakub Sikorski <jakub.sikorski0211@gmail.com>
2024-01-15 21:43:26 +0100

    fix

--- src/pages/blog/BlogMain.jsx
+++ src/pages/blog/BlogMain.jsx
@@ -8,6 +8,7 @@ import styled from "styled-components";
 const ArticlesWrapper = styled.div`
     display: flex;
     pagination: 30px;
+    flex-wrap: wrap;
     justify-content: space-evenly;
     margin-bottom: 20px;
 `
