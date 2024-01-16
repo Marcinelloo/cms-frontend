3f26c632996ca1020791dfeb703fb2029f4d1a49
bmikolajj <barcik.mikolaj1@gmail.com>
2024-01-16 13:15:45 +0100

    Car details finished

--- src/pages/car/Car.js
+++ src/pages/car/Car.js
@@ -1,10 +1,13 @@
 import { getCar } from "@/api/repositories/car";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useMutation } from "@tanstack/react-query";
 import styled from "styled-components";
-import store from "@/api/store";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { UserContext } from "@/common/context/userContext";
+import { useNavigate } from "react-router-dom";
+import Loading from "@/common/components/Loading";
+import Reservation from "@/pages/common/landing/components/Reservation";
+import Contact from "@/pages/common/landing/components/Contact";
 
 const PageContainer = styled.div`
   min-width: 400px;
@@ -16,24 +19,36 @@ const PageContainer = styled.div`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 `;
 
-const ImageWrapper = styled.div`
-  width: 100%;
-  max-height: 300px;
+const Wrapper = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const Image = styled.div`
+  max-width: 470px;
   overflow: hidden;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 
   img {
     width: 100%;
-    height: 100%;
+    height: auto;
     object-fit: cover;
   }
+`;
+
+const InfoBox = styled.div`
+  flex: 1;
+  font-size: 1.5rem;
+`;
+
+const PaddingWrapper = styled.div`
+  padding: 20px;
 `;
 
 const OpisHeader = styled.div`
   font-size: 1.5rem;
   font-weight: bold;
-  margin-bottom: 10px;
+  margin-bottom: 20px;
+  margin-top: 20px;
 `;
 
 const OpisText = styled.div`
@@ -41,34 +56,72 @@ const OpisText = styled.div`
 `;
 
 const CarInfoWrapper = styled.div`
-  padding: 20px;
-  background-color: #f8f8f8;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
 `;
 
 const ListSection = styled.div`
   display: flex;
   flex-direction: column;
   gap: 10px;
-  width: 50%; /* Each column takes 50% of the width */
+  width: 100%;
 `;
 
 const ListItem = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #fff;
+  background-color: #f8f8f8;
   padding: 10px;
   border-radius: 5px;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  width: calc(100% - 20px);
+`;
+
+const InfoName = styled.div`
+  display: flex;
+  max-width: calc(100% - 470px);
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 10px;
+
+  white-space: nowrap;
+`;
+
+const InfoPrice = styled.div`
+  font-size: 2rem;
+  margin-bottom: 10px;
+`;
+
+const ButtonSearch = styled.button`
+  width: auto;
+  position: relative;
+  display: inline-block;
+  flex: 0 0 auto;
+  overflow: hidden;
+  outline: none;
+  text-align: center;
+  text-decoration: none;
+  white-space: nowrap;
+  transition: all 0.3s ease 0s;
+  user-select: none;
+  cursor: pointer;
+  min-width: 120px;
+  height: 44px;
+  padding: 0px 16px;
+  color: rgb(255, 255, 255);
+  font-weight: 600;
+  font-size: 1rem;
+  text-transform: none;
+  background-color: rgb(174, 0, 0);
+  border: none;
+  border-radius: 15px;
 `;
 
 const CustomLink = styled(Link)``;
 
 const Car = () => {
   const [car, setCar] = useState({});
+  const [reservation, setReservation] = useState();
   const { id } = useParams();
+  const { user } = useContext(UserContext);
 
   const fetchCarMutation = useMutation({
     mutationFn: (value) => getCar(value),
@@ -81,73 +134,97 @@ const Car = () => {
     fetchCarMutation.mutate(id);
   }, []);
 
-  return fetchCarMutation.isLoading ? (
-    0
-  ) : (
-    <PageContainer>
-      <ImageWrapper>
-        <img
-          src={
-            process.env.REACT_APP_IMAGE_URL +
-            car.attributes?.image.data.attributes?.url
-          }
-        />
-      </ImageWrapper>
-      <OpisHeader>Opis</OpisHeader>
-      <OpisText>{car.attributes?.description}</OpisText>
+  return (
+    <>
+      {fetchCarMutation.isLoading && <Loading />}
+      <PageContainer>
+        <PaddingWrapper>
+          <Wrapper>
+            <Image>
+              <img
+                src={
+                  process.env.REACT_APP_IMAGE_URL +
+                  car.attributes?.image.data.attributes?.url
+                }
+              />
+            </Image>
+            <InfoBox>
+              <InfoName>
+                {car.attributes?.brand +
+                  " " +
+                  car.attributes?.model +
+                  " " +
+                  car.attributes?.year}
+              </InfoName>
+              <InfoPrice>{car.attributes?.price} zł</InfoPrice>
+              {user && (
+                <ButtonSearch onClick={() => setReservation(() => id)}>
+                  Zarezerwuj
+                </ButtonSearch>
+              )}
+            </InfoBox>
+          </Wrapper>
+          <OpisHeader>Opis</OpisHeader>
+          <OpisText>{car.attributes?.description}</OpisText>
 
-      <CarInfoWrapper>
-        <ListSection>
-          <ListItem>
-            <strong>Cena:</strong> {car.attributes?.price} zł
-          </ListItem>
-          <ListItem>
-            <strong>Kolor:</strong> {car.attributes?.color}
-          </ListItem>
-          <ListItem>
-            <strong>Marka:</strong> {car.attributes?.brand}
-          </ListItem>
-          <ListItem>
-            <strong>Model:</strong> {car.attributes?.model}
-          </ListItem>
-          <ListItem>
-            <strong>Rok produkcji:</strong> {car.attributes?.year}
-          </ListItem>
-          <ListItem>
-            <strong>Pojemność silnika:</strong>{" "}
-            {car.attributes?.engine_size + "l"}
-          </ListItem>
-          <ListItem>
-            <strong>Rodzaj paliwa:</strong> {car.attributes?.fuel_type}
-          </ListItem>
-          <ListItem>
-            <strong>Skrzynia biegów:</strong>{" "}
-            {car.attributes?.automatic_stick_shift
-              ? "Automatyczna"
-              : "Manualna"}
-          </ListItem>
-        </ListSection>
-        <ListSection>
-          <ListItem>
-            <strong>Liczba miejsc:</strong> {car.attributes?.seats}
-          </ListItem>
-          <ListItem>
-            <strong>Liczba drzwi:</strong> {car.attributes?.door_num}
-          </ListItem>
-          <ListItem>
-            <strong>Przebieg:</strong> {car.attributes?.przebieg + "km"}
-          </ListItem>
-          <ListItem>
-            <strong>Pierwszy właściciel:</strong>{" "}
-            {car.attributes?.first_owner ? "Tak" : "Nie"}
-          </ListItem>
-          <ListItem>
-            <strong>Bezwypadkowy:</strong>{" "}
-            {car.attributes?.bezwypadkowy ? "Tak" : "Nie"}
-          </ListItem>
-        </ListSection>
-      </CarInfoWrapper>
-    </PageContainer>
+          <CarInfoWrapper>
+            <ListSection>
+              <ListItem>
+                <strong>Rok produkcji:</strong> {car.attributes?.year}
+              </ListItem>
+              <ListItem>
+                <strong>Przebieg:</strong> {car.attributes?.przebieg + " km"}
+              </ListItem>
+
+              <ListItem>
+                <strong>Marka:</strong> {car.attributes?.brand}
+              </ListItem>
+              <ListItem>
+                <strong>Model:</strong> {car.attributes?.model}
+              </ListItem>
+            </ListSection>
+            <ListSection>
+              <ListItem>
+                <strong>Pojemność silnika:</strong>{" "}
+                {car.attributes?.engine_size + " l"}
+              </ListItem>
+              <ListItem>
+                <strong>Rodzaj paliwa:</strong> {car.attributes?.fuel_type}
+              </ListItem>
+              <ListItem>
+                <strong>Skrzynia biegów:</strong>{" "}
+                {car.attributes?.automatic_stick_shift
+                  ? "Automatyczna"
+                  : "Manualna"}
+              </ListItem>
+              <ListItem>
+                <strong>Kolor:</strong> {car.attributes?.color}
+              </ListItem>
+            </ListSection>
+            <ListSection>
+              <ListItem>
+                <strong>Liczba miejsc:</strong> {car.attributes?.seats}
+              </ListItem>
+              <ListItem>
+                <strong>Liczba drzwi:</strong> {car.attributes?.door_num}
+              </ListItem>
+              <ListItem>
+                <strong>Pierwszy właściciel:</strong>{" "}
+                {car.attributes?.first_owner ? "Tak" : "Nie"}
+              </ListItem>
+              <ListItem>
+                <strong>Bezwypadkowy:</strong>{" "}
+                {car.attributes?.bezwypadkowy ? "Tak" : "Nie"}
+              </ListItem>
+            </ListSection>
+          </CarInfoWrapper>
+        </PaddingWrapper>
+      </PageContainer>
+      <Contact />
+      {reservation && (
+        <Reservation data={reservation} setData={setReservation} />
+      )}
+    </>
   );
 };
 
--- src/pages/common/landing/components/SearchCars.js
+++ src/pages/common/landing/components/SearchCars.js
@@ -17,117 +17,116 @@ import { yearData } from "@/pages/user/cars/data/years";
 import { mileageRangeData } from "@/pages/user/cars/data/mileage";
 import { fuelTypeData } from "@/pages/user/cars/data/fuelType";
 
-
 const Wrapper = styled.div`
-    height: 450px;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    gap: 20px;
-    padding: 20px;
-    background: url(${Background}) center no-repeat;
-  `;
+  height: 450px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  padding: 20px;
+  background: url(${Background}) center no-repeat;
+`;
 
 const ButtonSearch = styled.button`
-    width: auto;
-    position: relative;
-    display: inline-block;
-    flex: 0 0 auto;
-    overflow: hidden;
-    outline: none;
-    text-align: center;
-    text-decoration: none;
-    white-space: nowrap;
-    transition: all 0.3s ease 0s;
-    user-select: none;
-    cursor: pointer;
-    min-width: 120px;
-    height: 44px;
-    padding: 0px 16px;
-    color: rgb(255, 255, 255);
-    font-weight: 600;
-    font-size: 1rem;
-    text-transform: none;
-    background-color: rgb(174, 0, 0);
-    border: none;
-    border-radius: 15px;
-  `;
+  width: auto;
+  position: relative;
+  display: inline-block;
+  flex: 0 0 auto;
+  overflow: hidden;
+  outline: none;
+  text-align: center;
+  text-decoration: none;
+  white-space: nowrap;
+  transition: all 0.3s ease 0s;
+  user-select: none;
+  cursor: pointer;
+  min-width: 120px;
+  height: 44px;
+  padding: 0px 16px;
+  color: rgb(255, 255, 255);
+  font-weight: 600;
+  font-size: 1rem;
+  text-transform: none;
+  background-color: rgb(174, 0, 0);
+  border: none;
+  border-radius: 15px;
+`;
 
 const ButtonWrapper = styled.div`
-    display: flex;
-    min-width: 800px;
-    align-items: center;
-    gap: 20px;
-    justify-content: right;
-  `;
+  display: flex;
+  min-width: 800px;
+  align-items: center;
+  gap: 20px;
+  justify-content: right;
+`;
 
 const SelectWrapper = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    gap: 20px;
-    max-width: 600px;
-  `;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  max-width: 600px;
+`;
 
 const ClearFilter = styled.div`
-    margin: 0px;
-    transition: color 0.3s ease 0s;
-    -webkit-font-smoothing: antialiased;
-    -webkit-tap-highlight-color: transparent;
-    color: rgb(255, 255, 255);
-    font-family: Montserrat, "DejaVu Sans", Verdana, sans‑serif;
-    letter-spacing: 0px;
-    font-weight: 500;
-    font-size: 1rem;
-    line-height: 1.5rem;
-    cursor: pointer;
-  `;
+  margin: 0px;
+  transition: color 0.3s ease 0s;
+  -webkit-font-smoothing: antialiased;
+  -webkit-tap-highlight-color: transparent;
+  color: rgb(255, 255, 255);
+  font-family: Montserrat, "DejaVu Sans", Verdana, sans‑serif;
+  letter-spacing: 0px;
+  font-weight: 500;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  cursor: pointer;
+`;
 
 const CarResultWrapper = styled.div`
-    display: flex;
-    justify-content: left;
-    margin: 40px;
+  display: flex;
+  justify-content: left;
+  margin: 40px;
 
-    transition: color 0.3s ease 0s;
-    -webkit-font-smoothing: antialiased;
-    -webkit-tap-highlight-color: transparent;
-    letter-spacing: 0px;
-    font-weight: 700;
-    font-size: 1.2rem;
-    line-height: 1.2rem;
-  `;
+  transition: color 0.3s ease 0s;
+  -webkit-font-smoothing: antialiased;
+  -webkit-tap-highlight-color: transparent;
+  letter-spacing: 0px;
+  font-weight: 700;
+  font-size: 1.2rem;
+  line-height: 1.2rem;
+`;
 
 const CarsWrapper = styled.div`
-    display: flex;
-    gap: 40px;
-    flex-wrap: wrap;
-    justify-content: center;
-    padding: 40px;
-    background-color: #f6f6f6;
-  `;
+  display: flex;
+  gap: 40px;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 40px;
+  background-color: #f6f6f6;
+`;
 
 const CarElement = styled.div`
-    width: 300px;
-    border: 1px dashed lightgray;
-  `;
+  width: 300px;
+  border: 1px dashed lightgray;
+`;
 
 const ImageWrapper = styled.div`
-    width: 300px;
-    height: 150px;
-    overflow: hidden;
+  width: 300px;
+  height: 150px;
+  overflow: hidden;
 
-    img {
-      width: 100%;
-      height: auto;
-    }
-  `;
+  img {
+    width: 100%;
+    height: auto;
+  }
+`;
 
 const CarInfoWrapper = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
-  `;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
 
 const PriceRangeWrapper = styled.div`
   display: flex;
@@ -135,8 +134,7 @@ const PriceRangeWrapper = styled.div`
   justify-content: space-between;
   width: 260px;
   color: white;
-  `;
-
+`;
 
 const SearchCars = () => {
   const minPriceRef = useRef();
@@ -186,26 +184,53 @@ const SearchCars = () => {
     },
   });
 
-
   const searchCarsMutationByParameter = useMutation({
-    mutationFn: (brand, model, fuel_type, year, mileage, minPrice, maxPrice) => findCarsByParameter(brand, model, fuel_type, year, mileage, minPrice, maxPrice),
+    mutationFn: (brand, model, fuel_type, year, mileage, minPrice, maxPrice) =>
+      findCarsByParameter(
+        brand,
+        model,
+        fuel_type,
+        year,
+        mileage,
+        minPrice,
+        maxPrice
+      ),
     onSuccess: ({ data }) => {
       setData(data.data);
     },
   });
-
 
   const handleSearch = () => {
     const brand = selectedBrand ? selectedBrand.value : null;
     const model = selectedModel ? selectedModel.value : null;
     const fuelType = selectedFuelType ? selectedFuelType.value : null;
     const year = selectedYear ? selectedYear.value : null;
-    const mileage = selectedMileage ? parseInt(selectedMileage.value, 10) : null;
-    const minPrice = minPriceRef?.current?.value && parseInt(minPriceRef.current.value);
-    const maxPrice = maxPriceRef?.current?.value && parseInt(maxPriceRef.current.value);
+    const mileage = selectedMileage
+      ? parseInt(selectedMileage.value, 10)
+      : null;
+    const minPrice =
+      minPriceRef?.current?.value && parseInt(minPriceRef.current.value);
+    const maxPrice =
+      maxPriceRef?.current?.value && parseInt(maxPriceRef.current.value);
 
-    if (selectedBrand || selectedBrand && selectedModel || selectedFuelType || selectedYear || mileage || minPrice || maxPrice) {
-      searchCarsMutationByParameter.mutate({ brand, model, fuelType, year, mileage, minPrice, maxPrice });
+    if (
+      selectedBrand ||
+      (selectedBrand && selectedModel) ||
+      selectedFuelType ||
+      selectedYear ||
+      mileage ||
+      minPrice ||
+      maxPrice
+    ) {
+      searchCarsMutationByParameter.mutate({
+        brand,
+        model,
+        fuelType,
+        year,
+        mileage,
+        minPrice,
+        maxPrice,
+      });
     }
   };
 
@@ -213,14 +238,13 @@ const SearchCars = () => {
     setSelectedBrand(brand);
     setSelectedModel(null);
     setModelOptions(() => {
-      return brand.models
-    })
-  }
+      return brand.models;
+    });
+  };
 
   useEffect(() => {
     searchCarsMutation.mutate();
   }, []);
-
 
   const handleDelete = () => {
     setSelectedBrand(null);
@@ -232,7 +256,6 @@ const SearchCars = () => {
     minPriceRef.current.value = null;
     maxPriceRef.current.value = null;
   };
-
 
   return (
     <>
@@ -259,7 +282,8 @@ const SearchCars = () => {
           />
           <PriceRangeWrapper>
             <div>
-              <Input type="number"
+              <Input
+                type="number"
                 labelStyle={{ display: "none" }}
                 placeholder="cena min"
                 width="100px"
@@ -267,14 +291,17 @@ const SearchCars = () => {
               />
             </div>
             <div>
-              <Input type="number"
+              <Input
+                type="number"
                 labelStyle={{ display: "none" }}
                 placeholder="cena max"
                 width="100px"
-                inputRef={maxPriceRef} />
+                inputRef={maxPriceRef}
+              />
             </div>
           </PriceRangeWrapper>
-          <SelectInput placeholder="przebieg"
+          <SelectInput
+            placeholder="przebieg"
             options={mileageRangeData}
             selected={selectedMileage}
             setSelected={setSelectedMileage}
